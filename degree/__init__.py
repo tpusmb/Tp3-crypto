66270e4d135c9ca02257997e0de5823e7c63b72c
ClemiNeko <c.lebras776@laposte.net>
2019-11-06 12:03:25 +0100

    Added custom card generator.

--- degree/__init__.py
+++ degree/__init__.py
@@ -2,5 +2,5 @@
 # -*- coding: utf-8 -*-
 
 from .stegano import decode_image, encode_image, char_generator
-from .utils import read_image, save_image
+from .utils import read_image, save_image, generate_degree_image
 from .signature import encrypt, decrypt
--- degree/utils.py
+++ degree/utils.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 import os
 import logging.handlers
 import cv2
+from .const import COMPANY_NAME
 
 PYTHON_LOGGER = logging.getLogger(__name__)
 if not os.path.exists("log"):
@@ -45,3 +46,50 @@ def save_image(image_path, image):
     except Exception as e:
         PYTHON_LOGGER.error("Error to save image: {}".format(e))
         return False
+
+
+def generate_degree_image(template, player_name, player_score):
+    """
+    Create the diploma from a template.
+    :param template: (Image) The template image of the diploma.
+    :param player_name: (String) The player's name.
+    :param player_score: (String) The player's score.
+    :return: Nothing
+    """
+    # Adding player's name
+    add_text_to_image(template, player_name, 210, 88, 1.8)
+    final_img = read_image("pokemon_card_with_text.png")
+
+    # Adding description
+    add_text_to_image(final_img, "Congratz!", 100, 660, 2)
+    add_text_to_image(final_img, "Now you're a true MLG membre!", 100, 720, 1)
+
+    # Adding score
+    add_text_to_image(final_img, "Your score", 450, 930, 1)
+    add_text_to_image(final_img, player_score, 470, 1015, 1.5)
+
+    # Adding company
+    add_text_to_image(final_img, COMPANY_NAME, 90, 930, 2.5)
+
+
+def add_text_to_image(image, text, x, y, font_size):
+    """
+    Put a text onto an image.
+    :param image: (Image) The image.
+    :param text: (String) The text.
+    :param x: (Integer) The x position of the text.
+    :param y: (Integer) The y position of the text.
+    :param font_size: (Float) The size of the text.
+    :return: Nothing
+    """
+    bottom_left_corner_of_text = (x, y)
+    font = cv2.FONT_HERSHEY_TRIPLEX
+    font_scale = font_size
+    font_color = (0, 0, 0)
+    line_type = 2
+
+    cv2.putText(image, text, bottom_left_corner_of_text, font, font_scale, font_color, line_type)
+    cv2.imwrite("pokemon_card_with_text.png", image)
+
+
+
