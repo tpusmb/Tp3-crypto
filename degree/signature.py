f51fa3a1d9309cc733519074fbe87225db0c91be
swan.blanc <swan.blanc@infomaniak.com>
2019-11-06 11:47:30 +0100

    use 512 sha

--- degree/signature.py
+++ degree/signature.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import
 import logging.handlers
 import os
 
-from Crypto.Hash import SHA256
+from Crypto.Hash import SHA3_512
 from Crypto.PublicKey import RSA
 from Crypto.Signature import pkcs1_15
 
@@ -29,13 +29,13 @@ FOLDER_ABSOLUTE_PATH = os.path.normpath(os.path.dirname(os.path.abspath(__file__
 
 def sign(path_private_key, message):
     key = RSA.import_key(open(path_private_key).read())
-    h = SHA256.new(message.encode())
+    h = SHA3_512.new(message.encode())
     return pkcs1_15.new(key).sign(h)
 
 
 def verify(path_public_key, message, signature):
     key = RSA.import_key(open(path_public_key).read())
-    h = SHA256.new(message.encode())
+    h = SHA3_512.new(message.encode())
     try:
         pkcs1_15.new(key).verify(h, signature)
         return True
