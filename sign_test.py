1ba5c33ea4c24e78bcd6e12b46955290140d85bd
swan.blanc <swan.blanc@infomaniak.com>
2019-11-06 11:46:56 +0100

    Add signature functions

--- sign_test.py
+++ sign_test.py
@@ -2,18 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
-
-import logging.handlers
 import os
-
-from Crypto.Hash import SHA256
-from Crypto.PublicKey import RSA
-from Crypto.Signature import pkcs1_15
+import logging.handlers
+from degree import sign, verify
 
 PYTHON_LOGGER = logging.getLogger(__name__)
 if not os.path.exists("log"):
     os.mkdir("log")
-HDLR = logging.handlers.TimedRotatingFileHandler("log/signature.log",
+HDLR = logging.handlers.TimedRotatingFileHandler("log/sign_test.log",
                                                  when="midnight", backupCount=60)
 STREAM_HDLR = logging.StreamHandler()
 FORMATTER = logging.Formatter("%(asctime)s %(filename)s [%(levelname)s] %(message)s")
@@ -26,19 +22,5 @@ PYTHON_LOGGER.setLevel(logging.DEBUG)
 # Absolute path to the folder location of this python file
 FOLDER_ABSOLUTE_PATH = os.path.normpath(os.path.dirname(os.path.abspath(__file__)))
 
-
-def sign(path_private_key, message):
-    key = RSA.import_key(open(path_private_key).read())
-    h = SHA256.new(message.encode())
-    return pkcs1_15.new(key).sign(h)
-
-
-def verify(path_public_key, message, signature):
-    key = RSA.import_key(open(path_public_key).read())
-    h = SHA256.new(message.encode())
-    try:
-        pkcs1_15.new(key).verify(h, signature)
-        return True
-    except Exception as e:
-        PYTHON_LOGGER.error("Error verify me signature: {}".format(e))
-        return False
+signature = sign("key.prev", "Pneedle")
+print(verify("key.pub", "Pneedle", signature))
