b1f7686ab69738acaafd18d1fa9c8294b2f12214
swan.blanc <swan.blanc@infomaniak.com>
2019-11-06 08:49:31 +0100

    add test image

--- image_test.py
+++ image_test.py
@@ -2,14 +2,16 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
-import os
+
 import logging.handlers
-import cv2
+import os
+
+from degree import read_image
 
 PYTHON_LOGGER = logging.getLogger(__name__)
 if not os.path.exists("log"):
     os.mkdir("log")
-HDLR = logging.handlers.TimedRotatingFileHandler("log/utils.log",
+HDLR = logging.handlers.TimedRotatingFileHandler("log/test_image.log",
                                                  when="midnight", backupCount=60)
 STREAM_HDLR = logging.StreamHandler()
 FORMATTER = logging.Formatter("%(asctime)s %(filename)s [%(levelname)s] %(message)s")
@@ -22,26 +24,8 @@ PYTHON_LOGGER.setLevel(logging.DEBUG)
 # Absolute path to the folder location of this python file
 FOLDER_ABSOLUTE_PATH = os.path.normpath(os.path.dirname(os.path.abspath(__file__)))
 
+img = read_image("chablais-orig.png")
 
-def read_image(image_path):
-    """
-
-    :param image_path:
-    :return: (ndarray) BGR image
-    """
-    return cv2.imread(image_path)
-
-
-def save_png_image(image_path, image):
-    """
-
-    :param image_path:
-    :param image:
-    :return:
-    """
-    try:
-        cv2.imwrite(image_path, image)
-        return True
-    except Exception as e:
-        PYTHON_LOGGER.error("Error to save image: {}".format(e))
-        return False
+for col in img:
+    for line in col:
+        b, g, r = line
